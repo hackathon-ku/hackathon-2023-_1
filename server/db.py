8cec95dd96b707ee4253c088180c563344a74d50
patthadon <beebobbob956@gmail.com>
2023-11-22 11:20:56 +0700

    server

---
+++ server/db.py
@@ -0,0 +1,33 @@
+# db.py
+
+import pymongo
+from bson import ObjectId  # Import ObjectId from bson
+from fastapi.encoders import jsonable_encoder
+import json
+
+def connect_to_mongodb():
+    # Replace with your MongoDB connection string
+    mongo_uri = "mongodb://localhost:27017/"
+    client = pymongo.MongoClient(mongo_uri)
+
+    # Specify the database name
+    database_name = "fast_api"
+
+    # Access the database
+    db = client[database_name]
+    docs = db["fastapi"].find({})
+    data = []
+    for doc in docs :
+        data.append(doc)
+    # print(data)
+    return client, db
+# connect_to_mongodb()
+
+def convert_to_json(doc):
+    """Recursively convert MongoDB documents to JSON-serializable format."""
+    for key, value in doc.items():
+        if isinstance(value, ObjectId):
+            doc[key] = str(value)
+        elif isinstance(value, dict):
+            doc[key] = convert_to_json(value)
+    return doc
--- server/server.py
+++ server/server.py
@@ -1,31 +1,27 @@
 from fastapi import FastAPI
-from bs4 import BeautifulSoup
 from fastapi.middleware.cors import CORSMiddleware
-import requests
+from db import connect_to_mongodb, convert_to_json
+import pymongo
 import json
 
-def gethour(number) :
-	url = f"http://nisit-ku.ku.ac.th/WebForm_report_std_B5.aspx?stdid={number}&link=1&ku=1"
-	print(url)
-	response = requests.get(url)
-	soup = BeautifulSoup(response.content, 'html.parser', from_encoding='utf-8')
 
-	name = soup.find(class_ = "header_fname_lname",id="LabelNAME").text
-	number = soup.find(id="LabelSTD_IDNO").text
-	faculty = soup.find(id="LabelSTD_FACULTY").text
-	activity_1 = soup.find(id="Label1_SCORE_SUM").text
-	activity_2 = soup.find(id="Label2_SCORE_SUM").text
-	activity_3 = soup.find(id="Label3_SCORE_SUM").text
-	# activity_4 = soup.find(id="Label4_SCORE_SUM").text
-	print(name)
-	print(number)
-	print(faculty)
-	print(activity_1)
-	print(activity_2)
-	print(activity_3)
-	data = {"name" : name,"number":number,"faculty":faculty,"activity1":activity_1,"activity2":activity_2,"activity3":activity_3}
-	print(data)
-	return data
+class CustomJSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        return jsonable_encoder(o)
+
+def gethour(number) :
+    client, database = connect_to_mongodb()
+    collection_name = "fastapi"
+    collection = database[collection_name]
+    result = collection.find({"id":number})
+    # print(result)
+    data = []
+    for doc in result :
+        data.append(doc)
+    # print(data[0])
+    result_json = [convert_to_json(data[0])]
+    client.close()
+    return result_json
 
 
 
