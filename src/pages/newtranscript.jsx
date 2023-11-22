a1a08c3eceb9f463231e63fff114633580b5b0aa
Patthadon Tantiameorn <beebobbob956@gmail.com>
2023-11-22 23:36:36 +0700

    fix newtranscript

--- src/pages/newtranscript.jsx
+++ src/pages/newtranscript.jsx
@@ -1,19 +1,19 @@
-import React, { useState,useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import backicon from '../picture/backicon.jpg'
 
 
-const Result = ({ a, b ,c}) => {
+const Result = ({ a, b, c }) => {
 	const counta = Object.keys(a).length;
 	const countb = Object.keys(b).length;
 	const countc = Object.keys(c).length;
-	let pass = (counta > 3 && countb > 4 && countc > 1)? true : false;
-	return(
+	let pass = (counta > 3 && countb > 4 && countc > 1) ? true : false;
+	return (
 		<div className='p-2'>
 			<div className={pass ? 'bg-[#2ec28c] flex justify-between rounded-t-lg' : 'bg-[#f188a2] flex justify-between rounded-t-lg'}>
 				<h4 className='pl-2 text-white'>Transcript กิจกรรมนิสิต</h4>
 			</div>
-			<div className={pass ? 'p-2 bg-[#2bb482] bg-opacity-20 text-[#2bb482] text-center text-2xl' :'p-2 bg-[#f188A2] bg-opacity-20 text-[#f188A2] text-center text-2xl'} >
-				{ pass ? <p>PASS</p> : <p>NOT PASS</p>}
+			<div className={pass ? 'p-2 bg-[#2bb482] bg-opacity-20 text-[#2bb482] text-center text-2xl' : 'p-2 bg-[#f188A2] bg-opacity-20 text-[#f188A2] text-center text-2xl'} >
+				{pass ? <p>PASS</p> : <p>NOT PASS</p>}
 			</div>
 		</div>
 
@@ -29,26 +29,26 @@ const Activity = ({ Title, required, activityList }) => {
 
 	const toggleBox = () => {
 		setShowBox(!showBox);
-		{toggleIcon == '+' ? setToggleIcon('x') : setToggleIcon('+')}
+		{ toggleIcon == '+' ? setToggleIcon('x') : setToggleIcon('+') }
 	}
 
-	const List = () =>{
+	const List = () => {
 
 		const sumOfValues = Object.values(activityList).reduce((sum, value) => sum + value, 0);
 
-		return(
+		return (
 			<div className='pt-2 pb-2'>
 				{Object.entries(activityList).map(([key, value]) => (
-					<div  className='flex justify-between pl-5 pr-5'>
+					<div className='flex justify-between pl-5 pr-5'>
 						<p className='w-fit'>{key}</p> <p className='w-fit'>{value} ชั่วโมง</p>
 					</div>
 				))}
-				{textColor 
-					? <p className='text-center pt-3'>รวม {count} กิจกรรม {sumOfValues} ชั่วโมง</p>  
-					: 	<div className='text-center pt-3'>
-							<p>ต้องการอีก {required-count} กิจกรรม</p>
-							<a className=' text-[#ff0000] underline'>ค้นหากิจกรรม</a>
-						</div>
+				{textColor
+					? <p className='text-center pt-3'>รวม {count} กิจกรรม {sumOfValues} ชั่วโมง</p>
+					: <div className='text-center pt-3'>
+						<p>ต้องการอีก {required - count} กิจกรรม</p>
+						<a className=' text-[#ff0000] underline'>ค้นหากิจกรรม</a>
+					</div>
 				}
 			</div>
 		)
@@ -61,11 +61,11 @@ const Activity = ({ Title, required, activityList }) => {
 				<div className='flex w-fit justify-between pr-2'>
 					{!textColor ? <p className="text-[#ffffff]">ไม่ผ่าน</p> : <p className="text-[#ffffff]">ผ่าน</p>}
 					<button className="pl-3 text-white" onClick={toggleBox}>{toggleIcon}</button>
-				</div>				
+				</div>
 			</div>
-			{ showBox && (
-				<div className={textColor ? 'bg-[#2bb482] bg-opacity-20' :' bg-[#f188A2] bg-opacity-20 '} >
-					<List className="w-full"/>
+			{showBox && (
+				<div className={textColor ? 'bg-[#2bb482] bg-opacity-20' : ' bg-[#f188A2] bg-opacity-20 '} >
+					<List className="w-full" />
 				</div>
 			)}
 		</div>
@@ -89,54 +89,55 @@ function newtranscript() {
 	const [name, setname] = useState("")
 	const [faculty, setfaculty] = useState("วิศวกรรมคอมพิวเตอร์")
 	const [check, setcheck] = useState("")
-	const [activityList1,setActivityList1] = useState({
-		'Pround to be KU':4,
-		'โครงการปฐมนิเทศนิสิตใหม่':4
+	const [activityList1, setActivityList1] = useState({
+		'Pround to be KU': 4,
+		'โครงการปฐมนิเทศนิสิตใหม่': 4
 	})
-	const [activityList2,setActivityList2] = useState({})
-	const [activityList3,setActivityList3] = useState({
-		'Pround to be KU':4,
-		'โครงการปฐมนิเทศนิสิตใหม่':4
+	const [activityList2, setActivityList2] = useState({})
+	const [activityList3, setActivityList3] = useState({
+		'Pround to be KU': 4,
+		'โครงการปฐมนิเทศนิสิตใหม่': 4
 	})
-    const fetchdata = async () => {
-        const response = await fetch(`http://localhost:8000/transcript/${studentNumber}`);
-        const data = await response.json();
-        setActivityList2(data[0].activity.activity2);
-        setActivityList1(data[0].activity.activity1);
-        setActivityList3(data[0].activity.activity3);
-        setname(data[0].name);
-        setfaculty(data[0].Faculty);
-        // console.log(data[0].name)
-    }
-    useEffect(()=> {
-        fetchdata();
-    },[])
+	const fetchdata = async () => {
+		const response = await fetch(`http://localhost:8000/transcript/${studentNumber}`);
+		const data = await response.json();
+		setActivityList2(data[0].activity.activity2);
+		setActivityList1(data[0].activity.activity1);
+		setActivityList3(data[0].activity.activity3);
+		setname(data[0].name);
+		setfaculty(data[0].Faculty);
+		// console.log(data[0].name)
+	}
+	useEffect(() => {
+		fetchdata();
+	}, [])
 
 	return (
-	<div className="flex justify-space-around font-kanit sm:flex-col">
-		<div className="p-5 flex-col bg-white min-h-screen w-1/2 border items-center justify-between sm:w-full">
-			<button>
-				<img
-				// src="https://via.placeholder.com/30"
-				src = { backicon }
-				alt="User Avatar"
-				className="rounded-full h-8 w-8 mr-2"
-				/>
-			</button>
-			<h1 className='bg-[#fad34f] text-black text-center  text-2xl p-5 rounded '>Activity Transcript</h1>
-			<br></br>
-			<Info studentNumber={studentNumber} name={name} faculty={faculty}/>
-			<Result a = {activityList1} b = {activityList2} c = {activityList3}/>
-		</div>
-		<div className="p-5 min-h-screen m-auto flex-col w-1/2 sm:w-full">
-			<Activity Title='กิจกรรมมหาวิทยาลัย' required={3} activityList={activityList1}/>
-			<Activity Title='กิจกรรมเพื่อเสริมสร้างสมรรถนะ' required={4} activityList={activityList2}/>
-			<Activity Title='กิจกรรมเพื่อสังคม' required={1} activityList={activityList3}/>
-			<div className='text-center mt-auto mb-auto'>
-				<button className='bg-[#6acb87] text-white text-center text-xl rounded-lg pt-1 pb-1 w-1/2'>กลับ</button>
+		<div className="flex justify-space-around font-kanit sm:flex-col">
+			<div className="p-5 flex-col bg-white w-1/2 items-center  justify-between sm:w-full">
+
+				<button>
+					<img
+						// src="https://via.placeholder.com/30"
+						src={backicon}
+						alt="User Avatar"
+						className="rounded-full h-8 w-8 mr-2"
+					/>
+				</button>
+				<h1 className='bg-[#fad34f] text-black text-center  text-2xl p-5 rounded '>Activity Transcript</h1>
+				<br></br>
+				<Info studentNumber={studentNumber} name={name} faculty={faculty} />
+				<Result a={activityList1} b={activityList2} c={activityList3} />
+			</div>
+			<div className="p-5 min-h-screen m-auto flex-col w-1/2 sm:w-full">
+				<Activity Title='กิจกรรมมหาวิทยาลัย' required={3} activityList={activityList1} />
+				<Activity Title='กิจกรรมเพื่อเสริมสร้างสมรรถนะ' required={4} activityList={activityList2} />
+				<Activity Title='กิจกรรมเพื่อสังคม' required={1} activityList={activityList3} />
+				<div className='text-center mt-auto mb-auto'>
+					<button className='bg-[#6acb87] text-white text-center text-xl rounded-lg pt-1 pb-1 w-1/2'>กลับ</button>
+				</div>
 			</div>
 		</div>
-	</div>
 
 	)
 }
