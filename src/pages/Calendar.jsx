28e5299e28e3c1b71132b702e3250d0b80b56fb3
patthadon <beebobbob956@gmail.com>
2023-11-23 19:46:40 +0700

    calendar

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -3,6 +3,8 @@ import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import moment from 'moment';
 import Filling from '../components/filling';
+import EventBox from '../components/Event_Box'
+import backicon from '../picture/backicon.jpg'
 //import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function ContinuousCalendar() {
@@ -113,9 +115,9 @@ function ContinuousCalendar() {
 	const Filter =()=>{
 		return(
 			<div className='flex top-0 left-0 absolute w-screen h-screen justify-center items-center   bg-black bg-opacity-70'>
-				<div className=' w-[40em] h-[40em] sm:w-[25rem] bg-white rounded-[20px]'>
-					<div className='header flex justify-evenly'>
-						<div className='flex-col justify-evenly'>
+				<div className='absolute w-[40em] h-[40em] sm:w-[25rem] bg-white rounded-[20px]'>
+					<div className='header flex justify-center'>
+						<div className='flex-col justify-center items'>
 							<div className='w-[400px] sm:w-[250px] h-[140px] flex flex-col justify-center items-center'>
 								{/* <div className='w-12 h-12 bg-yellow-500 rounded-full hover:bg-yellow-900 hover:duration-500 duration-500 cursor-pointer'  ></div> */}
 								<h1>วิทยาเขต</h1>
@@ -139,16 +141,20 @@ function ContinuousCalendar() {
 					<div className='main'></div>
 					<div className='footer flex justify-evenly m-10 mt-[120px]'>
 						<div className='w-[90px] flex justify-center'>
-							<button className='bg-green-500 w-[60px] h-[30px] rounded-[20px] hover:bg-green-600 hover:duration-500'>apply</button>
+							<button onClick={() => {setShowfilter(false)}} className='border border-black bg-white-200 w-[70px] h-[40px] rounded-[20px] duration-300 hover:bg-green-500 hover:duration-500'>apply</button>
 						</div>
 						<div className='w-[90px] flex justify-center'>
-							<button className='bg-red-500 w-[60px] h-[30px] rounded-[20px]  hover:bg-red-600 hover:duration-500'>reset</button>
+							<button onClick = {() => {
+                                setfilActi(["Select..."]);
+                                setfilCampus(["Select..."]);
+                                setfilFac(["Select..."]);
+                            }}className='border border-black bg-white-300 w-[70px] h-[40px] rounded-[20px] duration-200  hover:bg-red-600 hover:duration-500'>reset</button>
 						</div>
 					</div>
 				</div>
 				{/* <button className='top-[-300px] right-8 relative bg-red-600 rounded-full w-6 h-6 float-right hover:bg-red-800 hover:duration-500 duration-500' onClick={() => { setShowfilter(false) }}>x</button> */}
 				<button
-                        className="top-[-300px] right-8 relative text-gray-500 hover:text-gray-700"
+                        className="top-[-300px] right-[-300px] sm:right-[-175px] relative text-gray-500 hover:text-gray-700"
                         onClick={() => setShowfilter(false)}
                         >
                         <svg
@@ -173,23 +179,23 @@ function ContinuousCalendar() {
 
 	return (
 		<div className='font-kanit w-screen h-screen overflow-hidden'>
-			<div className='w-screen flex sm:flex-col '>
-				<div className='justify-center sm:w-full w-1/2 sm:h-1/2'>
+			<div className='w-screen flex sm:flex-col  justify-between ml-20 sm:ml-0'>
+				<div className='justify-center sm:w-full  sm:h-1/2'>
 					<div>
 						{isload && 
-							<div className='border p-10'>
+							<div className='p-10'>
 								<Calendar className="mx-auto" onChange={handleDateChange} value={selectedDate} tileClassName={tileContent}/>
 							</div>
 						}
 					</div>
-					<div className='flex justify-between px-3 w-[500px] mx-auto sm:w-full sm:px-10'>
-						<svg xmlns="http://www.w3.org/2000/svg" width="25" height="26" viewBox="0 0 25 26" fill="none" onClick={() => {
+					<div className='flex mb-5 justify-between px-3 w-[500px] mx-auto sm:w-full sm:px-10'>
+						<svg className='hover:cursor-pointer' xmlns="http://www.w3.org/2000/svg" width="25" height="26" viewBox="0 0 25 26" fill="none" onClick={() => {
 								setShowfilter(true);
 								console.log(showfilter);
 							}}>
 							<path d="M24.2188 19.7693H7.8125V18.8707C7.8125 18.3765 7.46094 17.9721 7.03125 17.9721H5.46875C5.03906 17.9721 4.6875 18.3765 4.6875 18.8707V19.7693H0.78125C0.351562 19.7693 0 20.1737 0 20.6679V22.4651C0 22.9594 0.351562 23.3637 0.78125 23.3637H4.6875V24.2623C4.6875 24.7566 5.03906 25.1609 5.46875 25.1609H7.03125C7.46094 25.1609 7.8125 24.7566 7.8125 24.2623V23.3637H24.2188C24.6484 23.3637 25 22.9594 25 22.4651V20.6679C25 20.1737 24.6484 19.7693 24.2188 19.7693ZM24.2188 10.7833H20.3125V9.88466C20.3125 9.39042 19.9609 8.98605 19.5312 8.98605H17.9688C17.5391 8.98605 17.1875 9.39042 17.1875 9.88466V10.7833H0.78125C0.351562 10.7833 0 11.1876 0 11.6819V13.4791C0 13.9733 0.351562 14.3777 0.78125 14.3777H17.1875V15.2763C17.1875 15.7705 17.5391 16.1749 17.9688 16.1749H19.5312C19.9609 16.1749 20.3125 15.7705 20.3125 15.2763V14.3777H24.2188C24.6484 14.3777 25 13.9733 25 13.4791V11.6819C25 11.1876 24.6484 10.7833 24.2188 10.7833ZM24.2188 1.79721H14.0625V0.898605C14.0625 0.404372 13.7109 0 13.2812 0H11.7188C11.2891 0 10.9375 0.404372 10.9375 0.898605V1.79721H0.78125C0.351562 1.79721 0 2.20158 0 2.69582V4.49303C0 4.98726 0.351562 5.39163 0.78125 5.39163H10.9375V6.29024C10.9375 6.78447 11.2891 7.18884 11.7188 7.18884H13.2812C13.7109 7.18884 14.0625 6.78447 14.0625 6.29024V5.39163H24.2188C24.6484 5.39163 25 4.98726 25 4.49303V2.69582C25 2.20158 24.6484 1.79721 24.2188 1.79721Z" fill="#2EC28C"/>
 						</svg>
-						<svg xmlns="http://www.w3.org/2000/svg" width="39" height="44" viewBox="0 0 39 44" fill="none">
+						<svg className='hover:cursor-pointer' xmlns="http://www.w3.org/2000/svg" width="39" height="44" viewBox="0 0 39 44" fill="none">
 						<path d="M10.9535 8.30237H6.53489C4.58262 8.30237 3 9.88499 3 11.8373V36.5815C3 38.5338 4.58262 40.1164 6.53489 40.1164H17.1396" stroke="#F188A2" stroke-width="5" stroke-linecap="round"/>
 						<path d="M23.3257 8.30237H27.7443C29.6966 8.30237 31.2792 9.88499 31.2792 11.8373V27.7443" stroke="#F188A2" stroke-width="5" stroke-linecap="round"/>
 						<path d="M10.0698 12.5442V9.18605C10.0698 8.69799 10.4655 8.30233 10.9535 8.30233C11.4416 8.30233 11.8447 7.9065 11.9283 7.42564C12.1902 5.9193 13.2053 3 17.1396 3C21.0739 3 22.089 5.9193 22.3509 7.42564C22.4345 7.9065 22.8377 8.30233 23.3257 8.30233C23.8136 8.30233 24.2094 8.69799 24.2094 9.18605V12.5442C24.2094 13.1299 23.7346 13.6047 23.1489 13.6047H11.1303C10.5446 13.6047 10.0698 13.1299 10.0698 12.5442Z" stroke="#F188A2" stroke-width="5" stroke-linecap="round"/>
@@ -202,27 +208,38 @@ function ContinuousCalendar() {
 					</div>	
 				</div>
 				{/* <div className='w-screen flex sm:flex-col sm:justify-normal justify-evenly h-screen'> */}
-				<div className='flex sm:flex-col max-h-screen sm:w-screen '>
+				<div className='flex  max-h-screen sm:w-screen mx-auto'>
 						
-					<div className='max-h-full sm:max-h-1/2 sm:w-screen'>
+					<div className=' flex sm:max-h-1/2 sm:w-screen'>
 						{selectedDate && (
-							<div className=''>
+							<div className=' p-3 flex-col overflow-auto'>
 								<h3>Events for {moment(selectedDate).format('MMMM DD, YYYY')}</h3>
 								{filteredEvents.length > 0 ? (
                                     <div className=' sm:w-screen flex overflow-auto '>
-                                        <ul className='w-screen sm:flex'> 
+                                        <ul className='sm:flex'> 
                                             {filteredEvents.map((event, index) => (
                                                 (checkeventacti(event) && checkeventcampus(event) && checkeventhost(event) &&
-                                                <li key={index} className='m-3'>
-                                                    <a href= {event.detail} className='w-[300px] h-[340px] bg-slate-200 block mb-[10px]'>
-                                                    <img src={event.image} className='w-[300px] h-[200px] object-cover' alt="" />
-                                                    <p>{event.name}</p> 
-                                                    <p>From {event.host.join(', ')}</p>
-                                                    <p>Activity Type : {event.activitytype.join(',')}</p>
-                                                    <p>Hour : {event.hour}</p>
-                                                    <p>campus : {event.campus}</p>
+                                                // <li key={index} className='m-3'>
+                                                //     <a href= {event.detail} className='w-[300px] h-[340px] bg-slate-200 block mb-[10px]'>
+                                                //     <img src={event.image} className='w-[300px] h-[200px] object-cover' alt="" />
+                                                //     <p>{event.name}</p> 
+                                                //     <p>From {event.host.join(', ')}</p>
+                                                //     <p>Activity Type : {event.activitytype.join(',')}</p>
+                                                //     <p>Hour : {event.hour}</p>
+                                                //     <p>campus : {event.campus}</p>
+                                                //     </a>
+                                                //     </li>
+                                                    <a href= {event.detail} className='m-1'>
+                                                        <EventBox className="w-[300px]" a
+                                                            activitytype={event.activitytype} 
+                                                            hour={event.hour} 
+                                                            campus={event.campus} 
+                                                            date = {moment(selectedDate).format('MMMM DD, YYYY')} 
+                                                            pic={event.image} 
+                                                            topic={event.name} 
+                                                            host={event.host.join(', ')} 
+                                                        />
                                                     </a>
-                                                    </li>
                                                 )
                                             ))}
                                         </ul>
--- src/pages/inputpage.jsx
+++ src/pages/inputpage.jsx
@@ -9,17 +9,19 @@ function inputpage() {
     const [activity, setActivity] = useState(['Select...']);
     const [hour,setHour] = useState();
     const [link,setLink] = useState();
+    const [campus,setCampus] = useState();
     const activitytypelist = ["กิจกรรมมหาวิทยาลัย","กิจกรรมเพื่อสังคม","กิจกรรมเสริมสร้างสมรรถภาพ"];
     const postevent = async(e) => {
         e.preventDefault();
         const requestData = {
             name: name,
-            dates: [startdate, enddate], // Adjust if both dates are needed
-            hosts: host,
+            date: [startdate, enddate], // Adjust if both dates are needed
+            host: host,
             activitytype: activity,
             hour: hour,
             image: "",
             detail:link,
+            campus:campus,
           };
         const response = await fetch('http://127.0.0.1:8000/addevent',{
             method : 'POST',
@@ -42,13 +44,15 @@ function inputpage() {
             <p>วันที่สิ้นสุด</p>
             <input type='date' className='border' value={enddate} onChange={(e) => {setEnddate(e.target.value)}}/>
             <p>ผู้จัด</p>
-            <Filling className = "border" color="bg-green-300" list = {["Sci","Eng"]} ans={host} setAns={setHost}/>
+            <Filling className = "border" color="bg-green-300" list = {["KUTech","Engineer","KUStartUP"]} ans={host} setAns={setHost}/>
             <p>ประเภทกิจกรรม</p>
             <Filling className = "border" color="bg-green-300" list = {activitytypelist} ans={activity} setAns={setActivity}/>
             <p>ชั่วโมงกิจกรรม</p>
             <input type="number" className='border' value={hour} onChange={(e) => {setHour(e.target.value)}}/>
             <p>ลิ้งค์กิจกรรม</p>
             <input type="text" className='border w-[400px]' value={link} onChange={(e) => {setLink(e.target.value)}} />
+            <p>วิทยาเขต</p>
+            <input type="text" className='border w-[400px]' value = {campus} onChange={(e) => {setCampus(e.target.value)}}/>
             <br />
             <button type="submit" onClick = {postevent}className='border mt-[50px]'>submit</button>
         </div>
