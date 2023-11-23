33be052ea1181d9686591b9cd706591f5890216a
kthanaporn <thanapornkitworakiat@gmail.com>
2023-11-23 12:19:20 +0700

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+import Newtranscript from './pages/newtranscript.jsx';
+import TopicSelectionMenu from './components/TopicSelectionMenu.jsx';
+import Mainpage from './pages/Mainpage.jsx';
+import Layout from "./pages/Layout";
+import CalendarKU from './pages/Calendar.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className='text-3xl'>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/NisitKU" element={<Layout />}>
+          <Route index element={<Mainpage />} />
+          <Route path="Newtranscript" element={<Newtranscript />} />
+		  <Route path="CalendarKU" element={<CalendarKU />} />
+          {/* <Route path="*" element={<NoPage />} /> */}
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
